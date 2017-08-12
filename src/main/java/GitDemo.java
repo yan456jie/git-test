865cd56c448ce9cf9e1cb4a1a3344e847fc1a302
yan456jie <yan456jie@qq.com>
2017-08-12 13:05:35 +0800

    uuuu

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -5,7 +5,6 @@ public class GitDemo {
     //main method
     public static void main(String[] args) {
         System.out.println("e");
-        System.out.println("e");
-        System.out.println("e");
+        System.out.println("eeee");
     }
 }
